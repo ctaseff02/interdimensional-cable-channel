6fcec5846a6bfb2328264970c54c8f5e7e2e985f
ctaseff <cet67@case.edu>
2023-02-21 02:45:14 -0500

    bot startup
    sssssssssssssssssss

--- Interdimensional Cable Channel/bot/bot.py
+++ Interdimensional Cable Channel/bot/bot.py
@@ -1,1 +1,19 @@
-# THERE'S NOTHING HERE LMAO
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+print(TOKEN)
+print(GUILD)
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
